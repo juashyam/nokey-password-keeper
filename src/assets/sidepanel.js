a88e26d6d4cf02e8f1c9558b9f0330ef3d1d8be4
juashyam <kumar.30.shyam@gmail.com>
2025-02-02 17:25:01 +0530

    Implement search, edit and delete

--- src/assets/sidepanel.js
+++ src/assets/sidepanel.js
@@ -36,8 +36,6 @@ document.addEventListener("alpine:init", () => {
         request.onsuccess = (event) => {
           db = event.target.result;
           this.dbConnection = db;
-          console.log(this.dbConnection);
-          console.log("Fetching all data from DB and render");
           this.initAppData();
         };
       },
@@ -57,45 +55,59 @@ document.addEventListener("alpine:init", () => {
       },
       // generate HTML for password DB
       renderRecord(record) {
-        let urlHtml = `<span>${record.name}</span>`;
+        let urlHtml = `<span class="name">${record.name}</span>`;
         if (record.url !== undefined && record.url !== null && record.url !== '') {
-          urlHtml = `<a href="${record.url}" target="_blank">${urlHtml}</a>`;
+          urlHtml = `<span class="name"><a href="${record.url}" target="_blank">${urlHtml}</a></span>`;
         }
         return `
         <div class="entry" spwid="${record.id}">
-          <div class="url">${urlHtml}</div>
+          <div class="field">
+            ${urlHtml}
+            <button class="edit-btn">
+              <span class="material-icons" x-on:click="editEntry" data-spkdata='${JSON.stringify(record)}'>edit</span>
+            </button>
+            <button class="delete-btn">
+              <span class="material-icons" x-on:click="deleteEntry" data-id="${record.id}">delete</span>
+            </button>
+          </div>
           <div class="field">
             <span>Username: ${record.user}</span>
             <button class="copy-btn">
-              <span class="material-icons" x-on:click="copytoclipboard" spwdata="${record.user}">content_copy</span>
+              <span class="material-icons" x-on:click="copytoclipboard" data-spkdata="${record.user}">content_copy</span>
             </button>
           </div>
           <div class="field">
             <span>Password: ${record.password}</span>
             <button class="copy-btn">
-              <span class="material-icons" x-on:click="copytoclipboard" spwdata="${record.password}">content_copy</span>
+              <span class="material-icons" x-on:click="copytoclipboard" data-spkdata="${record.password}">content_copy</span>
             </button>
           </div>
         </div>
         `;
       },
       // add new record
-      addNew(e) {
+      saveEntry(e) {
         let self = this;
         if (!self.validateForm()) {
           return;
         }
 
-        const pwListStore = self.dbConnection
-          .transaction("pwlist", "readwrite")
-          .objectStore("pwlist");
         let data = {
           name: document.getElementById('name').value,
           user: document.getElementById('user').value,
           password: document.getElementById('password').value,
           url: document.getElementById('url').value,
         };
-        let addRequest = pwListStore.add(data);
+
+        let id = document.getElementById('id').value;
+        if (id !== undefined && id !== null && id !== '') {
+          data['id'] = parseInt(id);
+        }
+
+        const pwListStore = self.dbConnection
+          .transaction("pwlist", "readwrite")
+          .objectStore("pwlist");
+        let addRequest = pwListStore.put(data);
         addRequest.onsuccess = function(event) {
           self.showSuccess = true;
           self.emptyForm();
@@ -121,6 +133,7 @@ document.addEventListener("alpine:init", () => {
       },
       // empty form
       emptyForm() {
+        document.getElementById('id').value = '';
         document.getElementById('name').value = '';
         document.getElementById('user').value = '';
         document.getElementById('password').value = '';
@@ -137,16 +150,59 @@ document.addEventListener("alpine:init", () => {
         this.showError = false;
         this.showSuccess = false;
         this.isModalOpen = false;
+        this.emptyForm();
       },
       // copy to clipboard
       copytoclipboard(e) {
         const copyButton = e.target;
-        var text = copyButton.getAttribute("spwdata");
+        var text = copyButton.getAttribute("data-spkdata");
         navigator.clipboard.writeText(text);
         copyButton.innerHTML = 'content_check';
         setTimeout(() => {
             copyButton.innerHTML = 'content_copy';
         }, 2000);
+      },
+      // search
+      search() {
+        const searchTerm = document.getElementById('search').value.toLowerCase();
+        const pwListStore = this.dbConnection
+          .transaction("pwlist", "readonly")
+          .objectStore("pwlist");
+
+        pwListStore.getAll().onsuccess = (event) => {
+          let pwList = event.target.result.reverse();
+          let filteredList = pwList.filter(record =>
+            record.name.toLowerCase().includes(searchTerm) ||
+            record.user.toLowerCase().includes(searchTerm) ||
+            (record.url && record.url.toLowerCase().includes(searchTerm))
+          );
+          this.spm_pwlist_html = filteredList.map(record => this.renderRecord(record)).join('');
+        };
+      },
+      // edit record
+      editEntry(e) {
+        let self = this;
+        var record = JSON.parse(e.target.getAttribute("data-spkdata"));
+        document.getElementById('id').value = record.id;
+        document.getElementById('name').value = record.name;
+        document.getElementById('user').value = record.user;
+        document.getElementById('password').value = record.password;
+        document.getElementById('url').value = record.url;
+        self.showModal();
+      },
+      // delete record
+      deleteEntry(e) {
+        if (window.confirm("Are you sure?")) {
+          let self = this;
+          var id = parseInt(e.target.getAttribute("data-id"));
+          const pwListStore = self.dbConnection
+            .transaction("pwlist", "readwrite")
+            .objectStore("pwlist");
+          const deleteRequest = pwListStore.delete(id);
+          deleteRequest.onsuccess = () => {
+            self.initAppData(); // Refresh the list after deletion
+          };
+        }
       },
     };
   });
